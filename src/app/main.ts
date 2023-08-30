363b0d5039655b9a5e3f71c35646a2e91e54312e
AxelJaBo <axel.910921@gmail.com>
2023-08-29 18:13:48 -0700

    Ejemplo de CRUD

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,43 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '12',
-    name: 'c1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  }
-})
+import { addProduct, products } from './products/product.service';
+
+for (let i = 0; i < 50; i++) {
+  faker.seed(i); // semilla para obtener datos consistentes
+
+  addProduct({
+    id: faker.string.uuid(),
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.url(),
+    color: faker.color.human(),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.multiple(faker.commerce.productAdjective, {
+      count: { min: 1, max: 5 },
+    }),
+    // Posible solución usando arrayElements
+    // tags: faker.helpers.arrayElements(
+    //   [
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //     faker.commerce.productAdjective(),
+    //   ],
+    //   { min: 1, max: 5 }
+    // ),
+    price: parseInt(faker.commerce.price({ min: 0, max: 150 }), 10),
+    createdAt: faker.date.past(),
+    updatedAt: faker.date.recent(),
+    stock: faker.number.int({ min: 0, max: 100 }),
+    size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+    category: {
+      id: 1,
+      name: faker.commerce.department(),
+      createdAt: faker.date.past(),
+      updatedAt: faker.date.recent(),
+    },
+  });
+}
+
+console.log(products);
